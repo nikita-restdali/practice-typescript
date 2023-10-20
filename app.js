11586c3e82108055a6b32d380f785a1a93017bb2
nikita-restdali <nikita@restdali.com>
2023-10-20 13:23:48 +0530

    started learning typescript

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var App = /** @class */ (function () {
+    function App() {
+    }
+    App.prototype.test = function () {
+        console.log("tset is working");
+    };
+    return App;
+}());
+let x = new App();
+x.test();
---
+++ app.ts
@@ -0,0 +1,10 @@
+export {}
+class App 
+{
+    test()
+    {
+        console.log("tset is working")
+    }
+}
+ let x = new App();
+ x.test()
