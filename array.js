5764cf35a48b6ea46095245c0d5cdf0adfde70df
nikita-restdali <nikita@restdali.com>
2023-10-21 13:24:20 +0530

    define array in typescript

---
+++ array.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var data = ['nik', 'mik', 'sik', 'zik', 'tik'];
+// let data = ['nik' , 'mik', 'sik' , 'zik', 'tik', 15, true];
+// let data:string= ['nik' , 'mik', 'sik' , 'zik', 'tik', 15, true]; //error because we only define string not told string of array
+// data.push('jik');   can push 
+// data.push(10000); only push if your array have different datatype
+data[7] = 'ram';
+console.warn(data);
